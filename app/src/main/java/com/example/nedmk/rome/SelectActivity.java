3f0babb4e100c8858d30d88c3370ae5e87d4c528
optima211 <nedmk@mail.ru>
2018-03-04 23:48:24 +0700

    new button home and not work to page updated

--- app/src/main/java/com/example/nedmk/rome/SelectActivity.java
+++ app/src/main/java/com/example/nedmk/rome/SelectActivity.java
@@ -1,13 +1,29 @@
 package com.example.nedmk.rome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SelectActivity extends AppCompatActivity {
+    private ImageButton homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+        homeButton = (ImageButton)findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(
+                new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.nedmk.rome.MainActivity");
+                        startActivity(intent);
+                    }
+
+                }
+        );
     }
 }
